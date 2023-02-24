100afb4aeda58098d080ecf8e9cd11f2e1d84408
hbfl3x <happybanda@dyuni.ac.mw>
2023-02-24 12:02:07 +0200

    rename environment variables

--- startups/express_middlewares.mjs
+++ startups/express_middlewares.mjs
@@ -5,7 +5,7 @@ import helmet from 'helmet'
 export default function express_middleware(app, express){
     app.use(express.json())
     app.use(express.urlencoded({extended: false}))
-    if(process.env.ENV === 'development') app.use(morgan())
+    if(process.env.NODE_ENV === 'development') app.use(morgan())
     app.use(compression())
     app.use(helmet())
 }
